304bb5c3747ee823991373b30017eeb1d7772ac8
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-09-10 14:54:47 +0200

    Implement deleting photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,4 +10,51 @@ class PhotosController < ApplicationController
   def new
     @photo = Photo.new
   end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    if @photo.update(photo_params)
+      redirect_to @photo
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    respond_to do |format|
+      if @photo.destroy
+        format.html { redirect_to photos_path, notice: 'Photo was deleted' }
+        format.js
+        format.json { render json: @photo, status: :ok, location: @photo }
+      else
+        format.html { render action: :index }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :description, :image)
+  end
 end
