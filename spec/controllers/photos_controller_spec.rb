e76deb6bee89764b280764dd75ae40b1889c4b43
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-09-11 10:47:48 +0200

    Add test for #new action

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -13,14 +13,37 @@ describe PhotosController do
       photo2 = create(:photo)
 
       get :index
-      expect(assigns(:photos)).to contain_exactly(photo1, photo2)
+      expect(assigns(:photos)).to include(photo1, photo2)
     end
   end
 
   describe 'GET #show' do
+    it 'responds succesfully and renders show template' do
+      photo = create(:photo)
+
+      get :show, params: { id: photo.id }
+      expect(response).to be_successful
+      expect(response).to render_template(:show)
+    end
+
+    it 'correctly assign photo to @photo' do
+      photo = create(:photo)
+      get :show, params: {id: photo.id }
+      expect(assigns(:photo)).to eq photo
+    end
   end
 
   describe 'GET #new' do
+    it 'responds succesfully and renders new template' do
+      get :new
+      expect(response).to be_successful
+      expect(response).to render_template(:new)
+    end
+
+    it 'assigns a new photo to @photo' do
+      get :new
+      expect(assigns(:photo)).to be_a_new(Photo)
+    end
   end
 
   describe 'GET #edit' do
